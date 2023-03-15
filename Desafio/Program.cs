22be9c757c36c2e3ba2e3e986a6d54ad459a30c6
Brian Souza <100374200+ProjetosSouza@users.noreply.github.com>
2023-03-15 15:40:43 -0300

    Add files via upload

---
+++ Desafio/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExercicioAlunos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Alunos aluno1 = new Alunos();
+        Console.Write("Digite seu nome ");
+        aluno1.nome= Console.ReadLine();
+        Console.Write("Digite a 1° nota: ");
+        aluno1.nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite a 2° nota: ");
+        aluno1.nota2 = Convert.ToDouble(Console.ReadLine());
+
+        aluno1.mensagem();
+        
+
+    }
+}
