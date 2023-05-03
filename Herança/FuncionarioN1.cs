aaa604fae622feb23cf38b72d7f17393bec42d0b
Brian Souza <100374200+ProjetosSouza@users.noreply.github.com>
2023-05-03 14:37:55 -0300

    03/05 Herança

---
+++ Herança/FuncionarioN1.cs
@@ -0,0 +1,10 @@
+namespace Herança;
+class FuncionarioN1:Funcionario
+{
+    
+
+    public override double Lucro(){
+        return base.Lucro()*0.01;
+    }
+
+}
---
+++ Herança/FuncionarioN3.cs
@@ -0,0 +1,10 @@
+namespace Herança;
+class FuncionarioN3:Funcionario
+{
+  
+  
+  public override double Lucro(){
+        return base.Lucro()*0.03;
+    }
+
+}
