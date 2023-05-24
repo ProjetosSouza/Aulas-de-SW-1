333ba1f36e4cfa36352855f70096613cb7f80703
Brian Souza <100374200+ProjetosSouza@users.noreply.github.com>
2023-05-24 14:55:50 -0300

    Add files via upload

---
+++ exercicio_2/Contrato_pessoa_fisica.cs
@@ -0,0 +1,19 @@
+﻿public class Contrato_pessoa_fisica:Contrato
+{
+    public string cpf;
+    public int idade;
+
+    public Contrato_pessoa_fisica(string detalhes,string email, string telefone, string nome, string cpf, int idade)
+        : base(detalhes, email, telefone, nome)
+    {
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    public override void exibir()
+    {
+        base.exibir();
+        Console.WriteLine("CPF : " + cpf);
+        Console.WriteLine("Idade : " + idade);
+    }
+}
