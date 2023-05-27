df808a6a1649ec485a462f6fda6cb749d606a1fd
Brian Souza <100374200+ProjetosSouza@users.noreply.github.com>
2023-05-27 10:16:54 -0300

    Delete Exercicio 3 directory

--- Exercicio 3/Fisico.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace exercicio3;
-class Fisico:Contrato
-{
-    public int Idade;
-    public override double CalcularPrestacao(int valor, int prazo)
-    {
-        return base.CalcularPrestacao(valor, prazo)+1;
-    }
-}
--- Exercicio 3/Juridico.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace exercicio3;
-class Juridico:Contrato
-{
-
-    public override double CalcularPrestacao(int valor , int prazo)
-    {
-        return base.CalcularPrestacao(valor, prazo)+3;
-    }
-}
